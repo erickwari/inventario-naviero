2666fb631a8d46500f7ed6be6932e02178c56bde
Erick <enfm2004@gmail.com>
2025-06-11 16:11:54 -0500

    en la pantalla del operador ya solo se muestran articulos de su buque

--- sistema/views.py
+++ sistema/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Articulo
+from .models import Articulo, Empleado, Inventario
 
 # Create your views here.
 
@@ -8,9 +8,23 @@ def login_view(request):
     # Lógica: validar usuario y contraseña, redirigir según rol
 
 def inventario_operador(request):
-    articulos = Articulo.objects.all()
-    return render(request, 'inventario_operador.html', {'articulos': articulos})
     # Lógica: mostrar tabla del inventario + botón “extraer artículo”
+    # Esto es solo temporal, para simular que el operador con ID 1 está logueado
+    operador = Empleado.objects.get(id_empleado=2)
+
+    # Buscamos su buque asignado
+    buque = operador.buque_asignado
+
+    # Obtenemos el inventario de ese buque
+    try:
+        inventario = Inventario.objects.get(buque=buque)
+        articulos = Articulo.objects.filter(inventario=inventario)
+    except Inventario.DoesNotExist:
+        articulos = []
+
+    return render(request, 'inventario_operador.html', {
+        'articulos': articulos
+    })
 
 def panel_admin(request):
     return render(request, 'panel_admin.html')
