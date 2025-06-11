1a5fcd3b2f13067d55c6c77a26a4599a06875af4
Erick <enfm2004@gmail.com>
2025-06-11 02:25:17 -0500

    Vistas conectadas con los HTML correspondientes

--- sistema/urls.py
+++ sistema/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
+    path('', views.login_view, name='login'),
+    path('operador/', views.inventario_operador, name='inventario_operador'),
+    path('admin/', views.panel_admin, name='panel_admin'),
+    path('registro/', views.registro, name='registro'),
+    path('eliminar/', views.confirmar_eliminacion, name='confirmar_eliminacion'),
 ]
--- sistema/views.py
+++ sistema/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'inicio.html')
+
+def confirmar_eliminacion(request):
+    return render(request, 'confirmar_eliminacion.html')
+    # Lógica: alerta de confirmación antes de eliminar artículo
